a4908a68edba6708f34bf4fed78155218e0dbc1a
ahmedaisar <ahmed.aisar@gmail.com>
2022-02-27 23:37:20 +0500

    wmvtest2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
 <script context="module">
-    import Login from "$lib/Login.svelte";
-    import Register from "$lib/Register.svelte";
-    // import { hotels } from "../stores/hotels";
-    import { goto } from "$app/navigation";
-import Hotel from "./maldives/resort/[hotel].svelte";
+ 
 
 
  
